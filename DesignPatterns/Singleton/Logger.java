a39b268ccf19bc67eb74187c3eae6513dfb0636f
Harshad Nawghare <harshadnawghare@gmail.com>
2025-07-28 20:44:13 +0530

    Added Singleton Pattern example with Logger, LogPrinter, and Main

---
+++ DesignPatterns/Singleton/Logger.java
@@ -0,0 +1,30 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Logger {
+
+    private List<String> L1;
+    private static  Logger instance;
+
+
+    private Logger () {
+        L1 = new ArrayList<>();
+    }
+
+    public static synchronized Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String messages) {
+        System.out.println(messages);
+        L1.add(messages);
+    }
+
+    public List<String> getLogs() {
+        return L1;
+    }
+
+}
